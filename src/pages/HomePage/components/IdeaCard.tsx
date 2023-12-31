3fee38c10ca8eeb3e94f7ab2a90997b47a412d79
rupeshkumar8825 <therupeshkumar8825@gmail.com>
2023-12-31 15:28:24 +0530

    enhanced the design for ideacard for this purpose. Added the option to share, upvote, comment and so on for this purpose.

--- src/pages/HomePage/components/IdeaCard.tsx
+++ src/pages/HomePage/components/IdeaCard.tsx
@@ -9,15 +9,17 @@ import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import CommentIcon from '@mui/icons-material/Comment';
+// import ShareIcon from '@mui/icons-material/Share';
 
-
-export default function ProjectCard() {
+export default function IdeaCard() {
 
 
   return (
@@ -38,7 +40,10 @@ export default function ProjectCard() {
             }}
             >
             <Typography  sx={{padding: 0, fontWeight: "bold", fontSize : "1.2rem"}}>Ttile of Idea </Typography>
-            <Typography  sx={{padding: 0,width: "40%", color : "#6B9DD7", fontWeight: "bold", fontSize : "0.6rem"}}> Updated on some time  </Typography>
+            {/* <Typography  sx={{padding: 0,width: "40%", color : "#6B9DD7", fontWeight: "bold", fontSize : "0.6rem"}}> Updated on some time  </Typography> */}
+            <Button variant="contained" size='small' sx={{backgroundColor: "blue", width : "13%", height: "30px",fontSize : "0.6rem"}}>
+            Show details 
+          </Button>
           </Box>
       
       <CardContent sx={{marginBottom : "1%", height : "120px"}}>
@@ -47,9 +52,11 @@ export default function ProjectCard() {
           Reiciendis autem natus dolor quidem culpa repudiandae facere debitis eos deleniti dolorem voluptas, aut magnam dignissimos at officiis mollitia fuga iusto eius quos! Cum soluta, quis nesciunt necessitatibus magnam pariatur!
           Nisi praesentium molestiae minima dignissimos, nihil veritatis obcaecati voluptatibus sapiente totam, atque eius numquam quod iste a quasi, voluptas distinctio laborum! Error nostrum animi cumque, reiciendis provident molestiae ducimus beatae.
         </Typography>
+        <Typography sx={{border : 0.5, borderColor : "lightblue", marginTop : "3%"}}></Typography>
       </CardContent>
       <CardActions disableSpacing>
-
+        
+        {/* <hr /> */}
         <Box
         component="main"
         sx={{
@@ -60,20 +67,86 @@ export default function ProjectCard() {
           bgcolor: "background.default",
           width: ``,
           padding:0,
+          marginTop : "2%"
         }}
         >
-          <Button variant="contained" size='small' sx={{backgroundColor: "blue", width : "13%", height: "30px",fontSize : "0.6rem"}}>
-            100 views 
-          </Button>
-          <Button variant="contained" size='small' sx={{backgroundColor: "blue", width : "10%", height: "30px",fontSize : "0.6rem"}}>
-            30 Upvotes
-          </Button>
-          <Button variant="contained" size='small' sx={{backgroundColor: "blue", width : "10%", height: "30px",fontSize : "0.6rem"}}>
-            Share
-          </Button>
-          <Button variant="contained" size='small' sx={{backgroundColor: "blue", width : "13%", height: "30px",fontSize : "0.6rem"}}>
-            Show details 
-          </Button>
+          <Box
+          component="main"
+          sx={{
+            display : "flex", 
+            justifyContent : "center", 
+            alignItems : "center",
+            flexGrow: 1,
+            bgcolor: "background.default",
+            width: ``,
+            padding:0,
+            // border : 2, 
+          }}
+          >
+            <RemoveRedEyeIcon></RemoveRedEyeIcon>
+            <Typography sx={{marginLeft : "15px"}}>
+              100 views 
+            </Typography>
+          </Box>
+          <Box
+          component="main"
+          sx={{
+            display : "flex", 
+            justifyContent : "center", 
+            alignItems : "center",
+            flexGrow: 1,
+            bgcolor: "background.default",
+            width: ``,
+            padding:0,
+            // border : 2, 
+          }}
+          >
+            <ThumbUpIcon></ThumbUpIcon>
+            
+            <Typography sx={{marginLeft : "15px"}}>
+              200 Upvotes 
+            </Typography>
+          </Box>
+          
+          <Box
+          component="main"
+          sx={{
+            display : "flex", 
+            justifyContent : "center", 
+            alignItems : "center",
+            flexGrow: 1,
+            bgcolor: "background.default",
+            width: ``,
+            padding:0,
+            // border : 2, 
+          }}
+          >
+            <ShareIcon></ShareIcon>
+            <Typography sx={{marginLeft : "15px"}}>
+              10 Share 
+            </Typography>
+          </Box>
+
+          <Box
+          component="main"
+          sx={{
+            display : "flex", 
+            justifyContent : "center", 
+            alignItems : "center",
+            flexGrow: 1,
+            bgcolor: "background.default",
+            width: ``,
+            padding:0,
+            // border : 2, 
+          }}
+          >
+            <CommentIcon></CommentIcon>
+            <Typography sx={{marginLeft : "15px"}}>
+              Comments
+            </Typography>
+          </Box>
+          
+          
 
         </Box>
        
