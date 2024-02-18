adbbffbd7dfc86ab8afb71288f2d4ecf01048d3f
rupeshkumar8825 <therupeshkumar8825@gmail.com>
2024-02-18 10:42:53 +0530

    Feature : Show project details section in detailed way or description

--- src/pages/IdeaDetailsPage/index.tsx
+++ src/pages/IdeaDetailsPage/index.tsx
@@ -9,20 +9,21 @@ import ShareIcon from '@mui/icons-material/Share';
 import imageThumbnail from "../../assets/thumb1.png";
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import FileDownloadDoneIcon from '@mui/icons-material/FileDownloadDone';
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getIdeaDetailsByIdApi } from "../../apis/IdeasRelatedApis";
+import { IIdeaDetails } from "../../interfaces/IdeaRelatedInterfaces";
 
 
 const IdeaDetailsPage = () => {
 
-    // fetch the list of ideas from react store 
-    const listOfIdeas =  useSelector((state : any) => state.ideaListReducer);
     const ideaId = useParams().ideaId;
-
     const [upvoted, setUpvoted] = useState<boolean>(false);
     const [shared, setShared] = useState<boolean>(false);
     const [addedToWatchList, setAddedToWatchList] = useState<boolean>(false);
+    const [ideaDetails, setIdeaDetails] = useState<IIdeaDetails>();
 
+
+    const dispatch = useDispatch();
     // handler for the component for this purpose 
     const onUpvoteIdeaHandler = () => {
         // here we have to toggle the value of the upvoted for this purpose 
@@ -50,6 +51,23 @@ const IdeaDetailsPage = () => {
     // callbacks comes here after hitting the apis to the backend 
     const getIdeaDetailsByIdApiCallback = (resultType : string, serverResponse : any) => {
         console.log("the response from the server after making the get idea details part is as follows \n", serverResponse);
+        if(serverResponse.response?.status && serverResponse.response.status === 500)
+		{
+			// do nothing for now 			
+		}
+		else if(serverResponse.response?.status && serverResponse.response.status === 401)
+		{
+			dispatch({type : "LOGOUT"});
+		}
+		else if(serverResponse.response?.status && serverResponse.response.status === 403)
+		{
+			// do nothing 
+		}
+		else if(serverResponse.status === 200)
+		{
+			const responseData = serverResponse.data.data;
+            setIdeaDetails(responseData);
+		}
     }
 
 
@@ -68,21 +86,12 @@ const IdeaDetailsPage = () => {
 
 
 
-    useEffect(() => {
-        if(listOfIdeas)
-        {
-            console.log("the list of idea is as follows  from the ideadetails page for this purpose \n", listOfIdeas);
-            // we have to find the details about this project for this purpose 
-        }
-    }, [listOfIdeas])
-    
-
 
     return (
         <>
         <Box sx={{ display: "flex", flexDirection: "column", justifyContent: 'center', alignItems: 'center', mt: `10%`, width: `83%`, marginLeft: `16%`, paddingLeft: 0, paddingRight: 0, marginBottom: "4%"}}>
             {/* box for topic  */}
-            <Box
+            {/* <Box
             component="main"
             sx={{
                 bgcolor: "background.default",
@@ -93,13 +102,13 @@ const IdeaDetailsPage = () => {
                 // border : 2
             }}
             >
-                <Typography sx={{ padding: 0.5, fontSize: "1.5em", textAlign: "center", backgroundColor: "#D9D9D9", borderRadius: 2 }}>Idea Details : Idea Name</Typography>
-            </Box>
+                <Typography sx={{ padding: 0.5, fontSize: "1.5em", textAlign: "center", backgroundColor: "#D9D9D9", borderRadius: 2 }}>Idea Details : {ideaDetails?.ideaName}</Typography>
+            </Box> */}
 
             
 
             {/* box for a field for username  */}
-            <Card sx={{width: "80%",  marginBottom: "3%", boxShadow: "7", borderColor : "black", borderRadius: "1%" }}>
+            <Card sx={{width: "70%",  marginBottom: "3%", boxShadow: "7", borderRadius: "1%"}}>
                 <Box 
                     component="span"
                     sx={{
@@ -116,19 +125,15 @@ const IdeaDetailsPage = () => {
                     // border : 2 
                     }}
                     >
-                    <Typography  sx={{padding: 0, fontWeight: "bold", fontSize : "1.2rem"}}> Idea1 </Typography>
+                    <Typography  sx={{padding: 0, fontWeight: "bold", fontSize : "1.2rem"}}> {ideaDetails?.ideaName} </Typography>
                     {addedToWatchList? <FileDownloadDoneIcon onClick={onAddToWatchListHandler} style={{height: "30px", width : "30px", color : "blue"}}></FileDownloadDoneIcon> : <ControlPointIcon onClick={onAddToWatchListHandler} style={{height: "30px", width : "30px"}}></ControlPointIcon>}
                 </Box>
                 
-                <CardContent sx={{marginBottom : "1%"}}>
-                    <Typography variant="body2" color="text.secondary">
-                    Here comes the description of the idea for this purpose. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni officiis illum saepe alias incidunt accusantium, sint laboriosam molestiae quos eaque enim autem perferendis non esse sit, tempore, sapiente quo aut.
-                    Nulla id vitae ratione facere iure suscipit maxime mollitia culpa delectus odit ex architecto voluptate provident, libero unde corporis hic minima illo sapiente magni quam repellat aspernatur necessitatibus! Libero, ipsa.
-                    Aliquam necessitatibus enim reprehenderit excepturi, quam quod accusamus officia fugit ullam itaque et! Impedit atque labore iure doloribus. Odio accusantium magnam repellendus ut animi laboriosam veniam eum quasi hic illum! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam commodi, quos dignissimos repellat maiores, ex odit ratione mollitia nesciunt eligendi esse doloremque tempore id nulla eos praesentium sint accusantium hic.
-                    Eius sint officia saepe cupiditate id, tempora voluptates quibusdam ipsum nemo ab laboriosam error nobis rerum consequuntur itaque dolore beatae quisquam vero non commodi pariatur iusto. Beatae consectetur neque eos?
-                    Magnam adipisci accusamus harum at incidunt aliquam labore debitis tenetur illo sapiente itaque vitae rerum ex placeat, a amet iure perferendis eaque sed exercitationem saepe, nisi aliquid architecto. Dolorum, optio.
+                <CardContent sx={{marginBottom : "1%", width : "100%", display : "flex", justifyContent : "center", alignItems : "center", flexDirection : 'column'}}>
+                    <Typography variant="body2" color="text.secondary" sx={{width : "90%"}}>
+                    {ideaDetails?.ideaDescription}
                     </Typography>
-                    <img  src={imageThumbnail} alt={"Loading"} style={{ height : "100%",width : "100%",  border : "4px", padding: "2%", borderColor : "black", marginTop: "2%"}} />
+                    <img  src={`http://localhost:4004/${ideaDetails?.thumbnail}`}  alt={"Loading"} style={{ height : "300px",width : "600px",  border : "4px", padding: "2%", borderColor : "black", marginTop: "2%"}} />
 
                     {/* <h1>what is happening here i dont know for this puropse </h1> */}
 
@@ -182,7 +187,7 @@ const IdeaDetailsPage = () => {
                     >
                         {upvoted? <ThumbUpIcon style={{color : "blue"}}/> : <ThumbUpIcon/>}
                         <Typography onClick={onUpvoteIdeaHandler}  sx={{marginLeft : "15px"}}>
-                        200 Upvotes 
+                        {ideaDetails?.upvotes.length} Upvotes 
                         </Typography>
                     </Box>
                     
@@ -201,7 +206,7 @@ const IdeaDetailsPage = () => {
                     >
                         <ShareIcon></ShareIcon>
                         <Typography onClick={onShareIdeaHandler}  sx={{marginLeft : "15px"}}>
-                        20 Share 
+                        {ideaDetails?.shared.length} Share 
                         </Typography>
                     </Box>
 
